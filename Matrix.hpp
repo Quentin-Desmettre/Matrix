0bdb32fa27758a7f6eb10acfcb459dbc6c139a3a
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-13 19:27:04 +0100

    [+] Line matrix

--- Matrix.hpp
+++ Matrix.hpp
@@ -102,6 +102,13 @@ namespace cppm
             memcpy(m_elems, array, m_size * sizeof(ElemType));
         }
 
+        explicit operator ElemType() const
+        {
+            if (m_size != 1)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, 1));
+            return m_elems[0];
+        }
+
         uint64 size(void) const {return m_size;}
 
         ElemType& operator = (const RowMatrix<ElemType> &other)
@@ -230,6 +237,190 @@ namespace cppm
             free(m_elems);
         }
     };
+
+    template <class ElemType>
+    class LineMatrix
+    {
+    private:
+        ElemType *m_elems;
+        uint64 m_size;
+
+    public:
+        LineMatrix(uint64 const& size, bool fill = false, ElemType const filler = ElemType())
+        {
+            m_size = size;
+            m_elems = (ElemType *)malloc(sizeof(ElemType) * m_size);
+
+            if (!m_elems)
+                throw std::bad_alloc();
+            if (fill)
+                for (uint64 i = 0; i < size; i++)
+                    m_elems[i] = filler;
+        }
+        LineMatrix(LineMatrix<ElemType> const& origin)
+        {
+            m_size = origin.m_size;
+            m_elems = (ElemType *)malloc(sizeof(ElemType) * m_size);
+
+            memcpy(m_elems, origin.m_elems, m_size * sizeof(ElemType));
+        }
+        LineMatrix(LineMatrix<ElemType> const *origin)
+        {
+            m_size = origin->m_size;
+            m_elems = (ElemType *)malloc(sizeof(ElemType) * m_size);
+
+            memcpy(m_elems, origin->m_elems, m_size * sizeof(ElemType));
+        }
+        LineMatrix(const ElemType array[])
+        {
+            m_size = sizeof(array) / sizeof(ElemType);
+            m_elems = (ElemType *)malloc(sizeof(ElemType) * m_size);
+
+            memcpy(m_elems, array, m_size * sizeof(ElemType));
+        }
+        LineMatrix(const ElemType *array, const uint64 &size)
+        {
+            m_size = size;
+            m_elems = (ElemType *)malloc(sizeof(ElemType) * m_size);
+
+            memcpy(m_elems, array, m_size * sizeof(ElemType));
+        }
+
+        explicit operator ElemType() const
+        {
+            if (m_size != 1)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, 1));
+            return m_elems[0];
+        }
+
+        uint64 size(void) const {return m_size;}
+
+        ElemType& operator = (const LineMatrix<ElemType> &other)
+        {
+            m_size = other.m_size;
+            m_elems = (ElemType *)realloc(m_elems, m_size);
+
+            memcpy(m_elems, other.m_elems, m_size * sizeof(ElemType));
+            return *this;
+        }
+        ElemType& operator = (const ElemType other[])
+        {
+            m_size = sizeof(other) / sizeof(ElemType);
+            m_elems = (ElemType *)realloc(m_elems, m_size);
+
+            memcpy(m_elems, other, m_size * sizeof(ElemType));
+            return *this;
+        }
+
+        ElemType& operator [] (uint64 const& i) const
+        {
+            if (i >= m_size)
+                throw std::out_of_range("Index too big: " + std::to_string(i) + ".");
+            return m_elems[i];
+        }
+
+        LineMatrix<ElemType> operator + (LineMatrix<ElemType> const& a) const
+        {
+            if (a.m_size != m_size)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, a.m_size));
+            LineMatrix<ElemType> result(a);
+
+            for (int i = 0; i < m_size; i++)
+                result.m_elems[i] += m_elems[i];
+            return result;
+        }
+        template <class T>
+        LineMatrix<ElemType> operator + (T const& n) const
+        {
+            if (m_size != 1)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, 1));
+            LineMatrix<ElemType> result(this);
+
+            result.m_elems[0] += n;
+
+            return result;
+        }
+        LineMatrix<ElemType> operator + (void) const
+        {
+            return LineMatrix<ElemType>(this);
+        }
+
+        LineMatrix<ElemType> operator - (LineMatrix<ElemType> const& a) const
+        {
+            if (a.m_size != m_size)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, a.m_size));
+            LineMatrix<ElemType> result(this);
+
+            for (int i = 0; i < m_size; i++)
+                result.m_elems[i] -= a[i];
+            return result;
+        }
+        template <class T>
+        LineMatrix<ElemType> operator - (T const& n) const
+        {
+            if (m_size != 1)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, 1));
+            LineMatrix<ElemType> result(this);
+
+            result.m_elems[0] -= n;
+
+            return result;
+        }
+        LineMatrix<ElemType> operator - (void) const
+        {
+            LineMatrix<ElemType> result(this);
+
+            for (int i = 0; i < m_size; i++)
+                result.m_elems[i] *= -1;
+            return result;
+        }
+
+        template <class T>
+        LineMatrix<ElemType> operator * (T const& n) const
+        {
+            LineMatrix<ElemType> result(this);
+
+            for (int i = 0; i < m_size; i++)
+                result.m_elems[i] *= n;
+            return result;
+        }
+
+        LineMatrix<ElemType> &operator += (LineMatrix<ElemType> const& a) const
+        {
+            if (a.m_size != m_size)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, a.m_size));
+            for (int i = 0; i < m_size; i++)
+                m_elems[i] += a.m_elems[i];
+            return *this;
+        }
+        LineMatrix<ElemType> &operator += (ElemType const& a) const
+        {
+            if (m_size != 1)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, 1));
+            m_elems[0] += a;
+            return *this;
+        }
+        LineMatrix<ElemType> &operator -= (LineMatrix<ElemType> const& a) const
+        {
+            if (a.m_size != m_size)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, a.m_size));
+            for (int i = 0; i < m_size; i++)
+                m_elems[i] -= a.m_elems[i];
+            return *this;
+        }
+        LineMatrix<ElemType> &operator -= (ElemType const& a) const
+        {
+            if (m_size != 1)
+                throw cppm::IncompatibleSizeException(m_size_t(1, m_size), m_size_t(1, 1));
+            m_elems[0] -= a;
+            return *this;
+        }
+
+        ~LineMatrix()
+        {
+            free(m_elems);
+        }
+    };
 }
 
 template <class T1, class T2>
@@ -239,6 +430,17 @@ cppm::RowMatrix<T1> operator + (T2 const a, cppm::RowMatrix<T1> const& b)
 }
 template <class T1, class T2>
 cppm::RowMatrix<T1> operator * (T2 const a, cppm::RowMatrix<T1> const& b)
+{
+    return b * a;
+}
+
+template <class T1, class T2>
+cppm::LineMatrix<T1> operator + (T2 const a, cppm::LineMatrix<T1> const& b)
+{
+    return b + a;
+}
+template <class T1, class T2>
+cppm::LineMatrix<T1> operator * (T2 const a, cppm::LineMatrix<T1> const& b)
 {
     return b * a;
 }
