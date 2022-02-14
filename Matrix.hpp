4d91e6ada3a5bc3a4122b5e6137b4fce9d70e704
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-14 18:51:15 +0100

    [+] New Matrix class, with constructors and operator=

---
+++ Matrix.hpp
@@ -0,0 +1,108 @@
+#ifndef E3096311_CEA5_475E_847E_7C8C54043D10
+#define E3096311_CEA5_475E_847E_7C8C54043D10
+
+#include <utility>
+#include <string>
+#include <cstring>
+
+namespace cppm
+{
+    typedef unsigned long uint64;
+    typedef long int64;
+    typedef uint64 size_t[3];
+
+    template <class Type>
+    class Matrix
+    {
+    private:
+        Type *_elems;
+        size_t _size;
+
+        void _copyPtr(Matrix<T> const *other)
+        {
+            _size[0] = other->_size[0];
+            _size[1] = other->_size[1];
+            _size[2] = other->_size[2];
+
+            std::memcpy(_elems, other->_elems, sizeof(T) * _size[2]);
+        }
+    public:
+        Matrix(uint64 const nb_line = 1, uint64 const nb_col = 1, bool const fill = false, Type const filler = Type())
+        {
+            if (!nb_line || !nb_col)
+                throw "Invalid null size";
+            _size[0] = nb_line;
+            _size[1] = nb_col;
+            _size[2] = nb_col * nb_line;
+
+            _elems = new Type[_size[2]];
+            if (fill)
+                for (uint64 i = 0, n = _size[2]; i < n; i++)
+                    _elems[i] = filler;
+        }
+        Matrix(Matrix<Type> const& other)
+        {
+            _copyPtr(&other);
+        }
+        Matrix(Matrix<Type> const *other)
+        {
+            _copyPtr(other);
+        }
+        ~Matrix()
+        {
+            delete [] _elems;
+        }
+
+        Matrix<Type>& operator=(Matrix<Type> const& other)
+        {
+            if (_size[2] != other._size[2]) {
+                delete [] _elems;
+                _elems = new Type[other._size[2]];
+            }
+            _copyPtr(&other);
+            return *this;
+        }
+        Matrix<Type>& operator=(Matrix<Type> const *other)
+        {
+            if (_size[2] != other->_size[2]) {
+                delete [] _elems;
+                _elems = new Type[other->_size[2]];
+            }
+            _copyPtr(other);
+            return *this;
+        }
+
+        const size_t& getSize(void) {return _size;}
+        Type &at(uint64 const i, uint64 const j)
+        {
+            if (j > _size[1])
+                throw "Bad column index: " + std::to_string(j);
+            if (i > _size[0])
+                throw "Bad row index: " + std::to_string(i);
+            return _elems[i * _size[0] + j];
+        }
+    };
+}
+
+/* operators:
+
+Todo:
+
++: Matrix
+-: Matrix
+*: Type2, Matrix
+/: Type2
+
++=: Matrix
+-=: Matrix
+*=: Matrix, Type2
+/=: Type2
+
+==: Matrix
+!=: Matrix
+
+Statics for identity
+
+*/
+
+#endif // E3096311_CEA5_475E_847E_7C8C54043D10
